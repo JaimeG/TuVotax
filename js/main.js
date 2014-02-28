59bd4a37c38d7de5689676403119cf31ea0b9200
Jaime Garay <jaime@joystickinteractive.com>
2014-02-28 02:04:44 -0600

    Plugin Faceboo, Twitter, y CSS en los plugins

--- js/main.js
+++ js/main.js
@@ -1,19 +1,10 @@
-window.fbAsyncInit = function() {
-  FB.init({
-    appId      : '815724718444105',
-    status     : true,
-    xfbml      : true
-  });
-};
-
-
-(function(d, s, id){
-   var js, fjs = d.getElementsByTagName(s)[0];
-   if (d.getElementById(id)) {return;}
-   js = d.createElement(s); js.id = id;
-   js.src = "http://connect.facebook.net/en_US/all.js";
-   fjs.parentNode.insertBefore(js, fjs);
- }(document, 'script', 'facebook-jssdk'));
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "http://connect.facebook.net/en_GB/all.js#xfbml=1&appId=815724718444105";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
 
 
 jQuery(document).ready(function($){
