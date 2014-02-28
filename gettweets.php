047b363b90e913ae18dcec675115f4b38d47f790
Diego Melendez <diedu89@gmail.com>
2014-02-28 02:59:34 -0600

    cambios pequeños

--- gettweets.php
+++ gettweets.php
@@ -2,7 +2,8 @@
 	session_start();
 	require_once("twitteroauth/twitteroauth.php"); //Path to twitteroauth library
 	
-	$notweets = 30;
+	$notweets["recent"] = 30;
+	$notweets["popular"] = 10;
 	$consumerkey = "yK8JOWxMao6uo49Vz7wLrQ";
 	$consumersecret = "9nXebFU3vlZpAB2uquuq9cZLO1d3nacQqujkWMI78Y";
 	$accesstoken = "97114532-v6mktzQJgKNQrPeo9ixPRaINOpuuHm18AmeUc2O9G";
@@ -12,10 +13,13 @@
 	  $connection = new TwitterOAuth($cons_key, $cons_secret, $oauth_token, $oauth_token_secret);
 	  return $connection;
 	}
-	 
+
+	$tipo = "recent";
+	if(isset($_GET['type'])) $tipo=$_GET['type'];
+
 	$connection = getConnectionWithAccessToken($consumerkey, $consumersecret, $accesstoken, $accesstokensecret);
 	 
-	$tweets = $connection->get("https://api.twitter.com/1.1/search/tweets.json?q=%23forouno&count=".$notweets."&result_type=recent");	 
+	$tweets = $connection->get("https://api.twitter.com/1.1/search/tweets.json?q=%23forouno&count=".$notweets[$tipo]."&result_type=mixed");
 	//$tweets = $connection->get("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=diedu89&count=".$notweets);
 	echo json_encode($tweets);
 ?>
--- js/twitterfeed.js
+++ js/twitterfeed.js
@@ -1,5 +1,9 @@
 $(document).ready(function () {
-   var displaylimit = 3;
+	setInterval(updateTweets,10000);     
+});
+
+function updateTweets(){
+ var displaylimit = 3;
 	var screenname = "#ForoUno";
    var showdirecttweets = false;
    var showretweets = true;
@@ -16,10 +20,9 @@ $(document).ready(function () {
 	
 	$('#twitter-feed').html(headerHTML + loadingHTML);
 	 
-    $.getJSON('gettweets.php',
+    $.getJSON('gettweets.php?',
         function(response) {
         		var feeds = response.statuses;   
-        		console.log(feeds);
 		   //alert(feeds);
             var feedHTML = '';
             var displayCounter = 1;         
@@ -121,52 +124,51 @@ $(document).ready(function () {
             }	
        		alert("error: " + error);
     });
-    
+}
 
-    //Function modified from Stack Overflow
-    function addlinks(data) {
-        //Add link to all http:// links within tweets
-         data = data.replace(/((https?|s?ftp|ssh)\:\/\/[^"\s\<\>]*[^.,;'">\:\s\<\>\)\]\!])/g, function(url) {
-            return '<a href="'+url+'"  target="_blank">'+url+'</a>';
-        });
-             
-        //Add link to @usernames used within tweets
-        data = data.replace(/\B@([_a-z0-9]+)/ig, function(reply) {
-            return '<a href="http://twitter.com/'+reply.substring(1)+'" style="font-weight:lighter;" target="_blank">'+reply.charAt(0)+reply.substring(1)+'</a>';
-        });
-		//Add link to #hastags used within tweets
-        data = data.replace(/\B#([_a-z0-9]+)/ig, function(reply) {
-            return '<a href="https://twitter.com/search?q='+reply.substring(1)+'" style="font-weight:lighter;" target="_blank">'+reply.charAt(0)+reply.substring(1)+'</a>';
-        });
-        return data;
-    }
-     
-     
-    function relative_time(time_value) {
-      var values = time_value.split(" ");
-      time_value = values[1] + " " + values[2] + ", " + values[5] + " " + values[3];
-      var parsed_date = Date.parse(time_value);
-      var relative_to = (arguments.length > 1) ? arguments[1] : new Date();
-      var delta = parseInt((relative_to.getTime() - parsed_date) / 1000);
-	  var shortdate = time_value.substr(4,2) + " " + time_value.substr(0,3);
-      delta = delta + (relative_to.getTimezoneOffset() * 60);
-     
-      if (delta < 60) {
-        return '1m';
-      } else if(delta < 120) {
-        return '1m';
-      } else if(delta < (60*60)) {
-        return (parseInt(delta / 60)).toString() + 'm';
-      } else if(delta < (120*60)) {
-        return '1h';
-      } else if(delta < (24*60*60)) {
-        return (parseInt(delta / 3600)).toString() + 'h';
-      } else if(delta < (48*60*60)) {
-        //return '1 day';
-		return shortdate;
-      } else {
-        return shortdate;
-      }
-    }
-     
-});
+//Function modified from Stack Overflow
+function addlinks(data) {
+  //Add link to all http:// links within tweets
+	data = data.replace(/((https?|s?ftp|ssh)\:\/\/[^"\s\<\>]*[^.,;'">\:\s\<\>\)\]\!])/g, function(url) {
+      return '<a href="'+url+'"  target="_blank">'+url+'</a>';
+  	});
+       
+  	//Add link to @usernames used within tweets
+  	data = data.replace(/\B@([_a-z0-9]+)/ig, function(reply) {
+      return '<a href="http://twitter.com/'+reply.substring(1)+'" style="font-weight:lighter;" target="_blank">'+reply.charAt(0)+reply.substring(1)+'</a>';
+  	});
+
+	//Add link to #hastags used within tweets
+  	data = data.replace(/\B#([_a-z0-9]+)/ig, function(reply) {
+      return '<a href="https://twitter.com/search?q='+reply.substring(1)+'" style="font-weight:lighter;" target="_blank">'+reply.charAt(0)+reply.substring(1)+'</a>';
+  	});
+  	return data;
+}
+
+
+function relative_time(time_value) {
+	var values = time_value.split(" ");
+	time_value = values[1] + " " + values[2] + ", " + values[5] + " " + values[3];
+	var parsed_date = Date.parse(time_value);
+	var relative_to = (arguments.length > 1) ? arguments[1] : new Date();
+	var delta = parseInt((relative_to.getTime() - parsed_date) / 1000);
+	var shortdate = time_value.substr(4,2) + " " + time_value.substr(0,3);
+	delta = delta + (relative_to.getTimezoneOffset() * 60);
+
+	if (delta < 60) {
+	  return '1m';
+	} else if(delta < 120) {
+	  return '1m';
+	} else if(delta < (60*60)) {
+	  return (parseInt(delta / 60)).toString() + 'm';
+	} else if(delta < (120*60)) {
+	  return '1h';
+	} else if(delta < (24*60*60)) {
+	  return (parseInt(delta / 3600)).toString() + 'h';
+	} else if(delta < (48*60*60)) {
+	  //return '1 day';
+	return shortdate;
+	} else {
+	  return shortdate;
+	}
+}
