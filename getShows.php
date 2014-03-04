eeb54fa77d8e6de65cbebd1b0c6cc4af5b760817
Diego Melendez <diedu89@gmail.com>
2014-03-04 04:54:43 -0600

    Funciona mostrar programacion en json por seleccion en array

---
+++ getShows.php
@@ -0,0 +1,366 @@
+<?php
+	$shows = array();
+	$programa = array(
+			'nombre' => 'Hora de aventura',
+			'inicio' => "0:0",
+			'fin'=>  "0:29",
+			'invitados' => array('finn el humano', 'jake el perro')
+		);
+	array_push($shows,$programa);
+	$programa = array(
+			'nombre' => 'Hora de aventura',
+			'inicio' => "0:30",
+			'fin'=>  "0:59",
+			'invitados' => array('finn el humano', 'jake el perro')
+		);
+	array_push($shows,$programa);
+	$programa = array(
+			'nombre' => 'Hora de aventura',
+			'inicio' => "1:0",
+			'fin'=>  "1:29",
+			'invitados' => array('finn el humano', 'jake el perro')
+		);
+	array_push($shows,$programa);
+	$programa = array(
+			'nombre' => 'Hora de aventura',
+			'inicio' => "1:30",
+			'fin'=>  "1:59",
+			'invitados' => array('finn el humano', 'jake el perro')
+		);
+	array_push($shows,$programa);
+	$programa = array(
+			'nombre' => 'Hora de aventura',
+			'inicio' => "2:0",
+			'fin'=>  "2:29",
+			'invitados' => array('finn el humano', 'jake el perro')
+		);
+	array_push($shows,$programa);
+	$programa = array(
+			'nombre' => 'Hora de aventura',
+			'inicio' => "2:30",
+			'fin'=>  "2:59",
+			'invitados' => array('finn el humano', 'jake el perro')
+		);
+	array_push($shows,$programa);
+	$programa = array(
+			'nombre' => 'Hora de aventura',
+			'inicio' => "3:0",
+			'fin'=>  "3:29",
+			'invitados' => array('finn el humano', 'jake el perro')
+		);
+	array_push($shows,$programa);
+	$programa = array(
+			'nombre' => 'Hora de aventura',
+			'inicio' => "3:30",
+			'fin'=>  "3:59",
+			'invitados' => array('finn el humano', 'jake el perro')
+		);
+	array_push($shows,$programa);
+	$programa = array(
+			'nombre' => 'Hora de aventura',
+			'inicio' => "4:0",
+			'fin'=>  "4:29",
+			'invitados' => array('finn el humano', 'jake el perro')
+		);
+	array_push($shows,$programa);
+	$programa = array(
+			'nombre' => 'Hora de aventura',
+			'inicio' => "4:30",
+			'fin'=>  "4:59",
+			'invitados' => array('finn el humano', 'jake el perro')
+		);
+	array_push($shows,$programa);
+	$programa = array(
+			'nombre' => 'Hora de aventura',
+			'inicio' => "5:0",
+			'fin'=>  "5:29",
+			'invitados' => array('finn el humano', 'jake el perro')
+		);
+	array_push($shows,$programa);
+	$programa = array(
+			'nombre' => 'Hora de aventura',
+			'inicio' => "5:30",
+			'fin'=>  "5:59",
+			'invitados' => array('finn el humano', 'jake el perro')
+		);
+	array_push($shows,$programa);
+	$programa = array(
+			'nombre' => 'Hora de aventura',
+			'inicio' => "6:0",
+			'fin'=>  "6:29",
+			'invitados' => array('finn el humano', 'jake el perro')
+		);
+	array_push($shows,$programa);
+	$programa = array(
+			'nombre' => 'Hora de aventura',
+			'inicio' => "6:30",
+			'fin'=>  "6:59",
+			'invitados' => array('finn el humano', 'jake el perro')
+		);
+	array_push($shows,$programa);
+	$programa = array(
+			'nombre' => 'Hora de aventura',
+			'inicio' => "7:0",
+			'fin'=>  "7:29",
+			'invitados' => array('finn el humano', 'jake el perro')
+		);
+	array_push($shows,$programa);
+	$programa = array(
+			'nombre' => 'Hora de aventura',
+			'inicio' => "7:30",
+			'fin'=>  "7:59",
+			'invitados' => array('finn el humano', 'jake el perro')
+		);
+	array_push($shows,$programa);
+	$programa = array(
+			'nombre' => 'Hora de aventura',
+			'inicio' => "8:0",
+			'fin'=>  "8:29",
+			'invitados' => array('finn el humano', 'jake el perro')
+		);
+	array_push($shows,$programa);
+	$programa = array(
+			'nombre' => 'Hora de aventura',
+			'inicio' => "8:30",
+			'fin'=>  "8:59",
+			'invitados' => array('finn el humano', 'jake el perro')
+		);
+	array_push($shows,$programa);
+	$programa = array(
+			'nombre' => 'Hora de aventura',
+			'inicio' => "9:0",
+			'fin'=>  "9:29",
+			'invitados' => array('finn el humano', 'jake el perro')
+		);
+	array_push($shows,$programa);
+	$programa = array(
+			'nombre' => 'Hora de aventura',
+			'inicio' => "9:30",
+			'fin'=>  "9:59",
+			'invitados' => array('finn el humano', 'jake el perro')
+		);
+	array_push($shows,$programa);
+	$programa = array(
+			'nombre' => 'Hora de aventura',
+			'inicio' => "10:0",
+			'fin'=>  "10:29",
+			'invitados' => array('finn el humano', 'jake el perro')
+		);
+	array_push($shows,$programa);
+	$programa = array(
+			'nombre' => 'Hora de aventura',
+			'inicio' => "10:30",
+			'fin'=>  "10:59",
+			'invitados' => array('finn el humano', 'jake el perro')
+		);
+	array_push($shows,$programa);
+	$programa = array(
+			'nombre' => 'Hora de aventura',
+			'inicio' => "11:0",
+			'fin'=>  "11:29",
+			'invitados' => array('finn el humano', 'jake el perro')
+		);
+	array_push($shows,$programa);
+	$programa = array(
+			'nombre' => 'Hora de aventura',
+			'inicio' => "11:30",
+			'fin'=>  "11:59",
+			'invitados' => array('finn el humano', 'jake el perro')
+		);
+	array_push($shows,$programa);
+	$programa = array(
+			'nombre' => 'Hora de aventura',
+			'inicio' => "12:0",
+			'fin'=>  "12:29",
+			'invitados' => array('finn el humano', 'jake el perro')
+		);
+	array_push($shows,$programa);
+	$programa = array(
+			'nombre' => 'Hora de aventura',
+			'inicio' => "12:30",
+			'fin'=>  "12:59",
+			'invitados' => array('finn el humano', 'jake el perro')
+		);
+	array_push($shows,$programa);
+	$programa = array(
+			'nombre' => 'Hora de aventura',
+			'inicio' => "13:0",
+			'fin'=>  "13:29",
+			'invitados' => array('finn el humano', 'jake el perro')
+		);
+	array_push($shows,$programa);
+	$programa = array(
+			'nombre' => 'Hora de aventura',
+			'inicio' => "13:30",
+			'fin'=>  "13:59",
+			'invitados' => array('finn el humano', 'jake el perro')
+		);
+	array_push($shows,$programa);
+	$programa = array(
+			'nombre' => 'Hora de aventura',
+			'inicio' => "14:0",
+			'fin'=>  "14:29",
+			'invitados' => array('finn el humano', 'jake el perro')
+		);
+	array_push($shows,$programa);
+	$programa = array(
+			'nombre' => 'Hora de aventura',
+			'inicio' => "14:30",
+			'fin'=>  "14:59",
+			'invitados' => array('finn el humano', 'jake el perro')
+		);
+	array_push($shows,$programa);
+	$programa = array(
+			'nombre' => 'Hora de aventura',
+			'inicio' => "15:0",
+			'fin'=>  "15:29",
+			'invitados' => array('finn el humano', 'jake el perro')
+		);
+	array_push($shows,$programa);
+	$programa = array(
+			'nombre' => 'Hora de aventura',
+			'inicio' => "15:30",
+			'fin'=>  "15:59",
+			'invitados' => array('finn el humano', 'jake el perro')
+		);
+	array_push($shows,$programa);
+	$programa = array(
+			'nombre' => 'Hora de aventura',
+			'inicio' => "16:0",
+			'fin'=>  "16:29",
+			'invitados' => array('finn el humano', 'jake el perro')
+		);
+	array_push($shows,$programa);
+	$programa = array(
+			'nombre' => 'Hora de aventura',
+			'inicio' => "16:30",
+			'fin'=>  "16:59",
+			'invitados' => array('finn el humano', 'jake el perro')
+		);
+	array_push($shows,$programa);
+	$programa = array(
+			'nombre' => 'Hora de aventura',
+			'inicio' => "17:0",
+			'fin'=>  "17:29",
+			'invitados' => array('finn el humano', 'jake el perro')
+		);
+	array_push($shows,$programa);
+	$programa = array(
+			'nombre' => 'Hora de aventura',
+			'inicio' => "17:30",
+			'fin'=>  "17:59",
+			'invitados' => array('finn el humano', 'jake el perro')
+		);
+	array_push($shows,$programa);
+	$programa = array(
+			'nombre' => 'Hora de aventura',
+			'inicio' => "18:0",
+			'fin'=>  "18:29",
+			'invitados' => array('finn el humano', 'jake el perro')
+		);
+	array_push($shows,$programa);
+	$programa = array(
+			'nombre' => 'Hora de aventura',
+			'inicio' => "18:30",
+			'fin'=>  "18:59",
+			'invitados' => array('finn el humano', 'jake el perro')
+		);
+	array_push($shows,$programa);
+	$programa = array(
+			'nombre' => 'Hora de aventura',
+			'inicio' => "19:0",
+			'fin'=>  "19:29",
+			'invitados' => array('finn el humano', 'jake el perro')
+		);
+	array_push($shows,$programa);
+	$programa = array(
+			'nombre' => 'Hora de aventura',
+			'inicio' => "19:30",
+			'fin'=>  "19:59",
+			'invitados' => array('finn el humano', 'jake el perro')
+		);
+	array_push($shows,$programa);
+	$programa = array(
+			'nombre' => 'Hora de aventura',
+			'inicio' => "20:0",
+			'fin'=>  "20:29",
+			'invitados' => array('finn el humano', 'jake el perro')
+		);
+	array_push($shows,$programa);
+	$programa = array(
+			'nombre' => 'Hora de aventura',
+			'inicio' => "20:30",
+			'fin'=>  "20:59",
+			'invitados' => array('finn el humano', 'jake el perro')
+		);
+	array_push($shows,$programa);
+	$programa = array(
+			'nombre' => 'Hora de aventura',
+			'inicio' => "21:0",
+			'fin'=>  "21:29",
+			'invitados' => array('finn el humano', 'jake el perro')
+		);
+	array_push($shows,$programa);
+	$programa = array(
+			'nombre' => 'Hora de aventura',
+			'inicio' => "21:30",
+			'fin'=>  "21:59",
+			'invitados' => array('finn el humano', 'jake el perro')
+		);
+	array_push($shows,$programa);
+	$programa = array(
+			'nombre' => 'Hora de aventura',
+			'inicio' => "22:0",
+			'fin'=>  "22:29",
+			'invitados' => array('finn el humano', 'jake el perro')
+		);
+	array_push($shows,$programa);
+	$programa = array(
+			'nombre' => 'Hora de aventura',
+			'inicio' => "22:30",
+			'fin'=>  "22:59",
+			'invitados' => array('finn el humano', 'jake el perro')
+		);
+	array_push($shows,$programa);
+	$programa = array(
+			'nombre' => 'Hora de aventura',
+			'inicio' => "23:0",
+			'fin'=>  "23:29",
+			'invitados' => array('finn el humano', 'jake el perro')
+		);
+	array_push($shows,$programa);
+	$programa = array(
+			'nombre' => 'Hora de aventura',
+			'inicio' => "23:30",
+			'fin'=>  "23:59",
+			'invitados' => array('finn el humano', 'jake el perro')
+		);
+
+$actual = null;
+$siguiente = null;
+
+$indice=0;
+$ahora=strtotime(date('h:i'));
+
+
+// while($siguiente == null || $actual == null){
+// 	if()
+// }
+$i=0;
+while(($actual==null || $siguiente==null) && $i < count($shows)){
+	$programa = $shows[$i];
+	if( ( $ahora >= strtotime($programa["inicio"]) ) && ( $ahora <= strtotime($programa["fin"]) )){
+		$actual = $programa;
+		$siguiente = $shows[$i++];
+		break;
+	}
+	$i++;
+}
+
+echo json_encode(
+	array(
+		"hora_servidor"=>date("h:i"), 
+		"actual"=>$actual,
+		"siguiente"=>$siguiente
+		)
+	);
