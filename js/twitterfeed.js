7aeebfdcf1ad2111b309528acfa702a20f34790b
Diego Melendez <diedu89@gmail.com>
2014-03-07 12:19:52 -0600

    cambio en el tiempo de refresh de tweets y detalles del feed

--- js/twitterfeed.js
+++ js/twitterfeed.js
@@ -32,7 +32,7 @@ function scrollPopular(){
 
 function updateFeed(){
  	var displaylimit = 30;
-	var screenname = "#ForoUno";
+	var screenname = "#TuVotasTvx";
    var showdirecttweets = false;
    var showretweets = true;
    var showtweetlinks = true;
