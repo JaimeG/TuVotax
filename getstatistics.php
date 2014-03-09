0ee4aa952a39ebb1f5018f8f7cc602a92418cd2a
Diego Melendez <diedu89@gmail.com>
2014-03-09 16:19:16 -0600

    cambios para obtener mas datos del tse

--- getstatistics.php
+++ getstatistics.php
@@ -6,15 +6,33 @@
     if(!$dom->loadHTML(curl("http://preliminar2014.tse.gob.sv/resultados/99/DPR999999.htm")))
         die('error');
 
+    $procesadas = $dom->getElementById('res_pjrvesc_val');
+    $noprocesadas = $dom->getElementById('res_jrvnoesc_val');
+    $hora = $dom->getElementById('xhora');
+
     $fmln = $dom->getElementById('P001');
     $arena = $dom->getElementById('P005');
+
     $caracteres = array("\t", "\r", "\n", " ", "%");
     $fmln = str_replace($caracteres, "", $fmln->nodeValue);
     $arena = str_replace($caracteres, "", $arena->nodeValue);
-    
+    $procesadas = str_replace($caracteres, "", $procesadas->nodeValue);
+    $noprocesadas = str_replace($caracteres, "", $noprocesadas->nodeValue);
+    $hora = str_replace($caracteres, "", $hora);
+
     $file = "statistics.json";
     $fh = fopen($file, 'w') or die("can't open file");
-    fwrite($fh, json_encode(array('fmln' => $fmln, 'arena'=>$arena )));
+    fwrite($fh, json_encode(
+                    array(
+                        'fmln' => $fmln, 
+                        'arena'=>$arena, 
+                        'procesadas' => $procesadas, 
+                        'noprocesadas'=>$noprocesadas,
+                        'hora'=>$hora
+                        )
+                    )
+            );
+    
     fclose($fh);
 
     // Defining the basic cURL function
